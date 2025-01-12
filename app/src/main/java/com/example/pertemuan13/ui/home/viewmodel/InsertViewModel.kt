66ffe57ed744207e0e3d2c70e6e3a55a835f40d1
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2025-01-12 22:22:56 +0700

    validasi data input pengguna

--- app/src/main/java/com/example/pertemuan13/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan13/ui/home/viewmodel/InsertViewModel.kt
@@ -26,3 +26,80 @@ class InsertViewModel (
         )
     }
 
+
+    fun validateFields(): Boolean{
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isEmpty()) "NIM Tidak Boleh Kosong" else null,
+            nama = if (event.nama.isEmpty()) "Nama Tidak Boleh Kosong" else null,
+            alamat = if (event.alamat.isEmpty()) "Alamat Tidak Boleh Kosong" else null,
+            jenisKelamin = if (event.jenisKelamin.isEmpty()) "Jenis Kelamin Tidak Boleh Kosong" else null,
+            kelas = if (event.kelas.isEmpty()) "Kelas Tidak Boleh Kosong" else null,
+            angkatan = if (event.angkatan.isEmpty()) "Angkatan Tidak Boleh Kosong" else null
+        )
+
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun insertMhs() {
+        if (validateFields()) {
+            viewModelScope.launch {
+                uiState = FormState.Loading
+                try {
+                    mhs.insertMhs(uiEvent.insertUiEvent.toMhsModel())
+                    uiState = FormState.Success("Data Berhasil Disimpan")
+                } catch (e: Exception) {
+                    uiState = FormState.Error("Data Gagal Disimpan")
+                }
+            }
+        } else {
+            uiState = FormState.Error("Data Tidak Valid")
+        }
+    }
+
+    fun resetForm() {
+        uiEvent = InsertUiState()
+        uiState = FormState.Idle
+    }
+
+    fun resetSnackBarMessage() {
+        uiState = FormState.Idle
+    }
+}
+
+sealed class FormState {
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Success(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+}
+
+data class InsertUiState (
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState()
+)
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val alamat: String? = null,
+    val jenisKelamin: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && alamat == null &&
+                jenisKelamin == null && kelas == null && angkatan == null
+    }
+}
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
