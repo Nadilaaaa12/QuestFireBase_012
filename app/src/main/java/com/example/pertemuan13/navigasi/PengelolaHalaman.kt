3b18f2eb54945e5188148ce47fb0e5132aa58f53
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2025-01-12 21:58:22 +0700

    Menyiapkan host navigasi dengan tujuan awal

---
+++ app/src/main/java/com/example/pertemuan13/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,22 @@
+package com.example.pertemuan13.navigasi
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan13.ui.home.pages.HomeScreen
+import com.example.pertemuan13.ui.home.pages.InsertMhsView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
