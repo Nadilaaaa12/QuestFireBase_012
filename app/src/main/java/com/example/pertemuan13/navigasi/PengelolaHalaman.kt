9d79c98cc345557bd66d5325bdabb5d1a73d2b60
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2025-01-15 17:04:30 +0700

    Melengkapi di pengelola halaman

--- app/src/main/java/com/example/pertemuan13/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan13/navigasi/PengelolaHalaman.kt
@@ -4,9 +4,12 @@ package com.example.pertemuan13.navigasi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pertemuan13.ui.home.pages.DetailMhsView
 import com.example.pertemuan13.ui.home.pages.HomeScreen
 import com.example.pertemuan13.ui.home.pages.InsertMhsView
 
@@ -35,6 +38,24 @@ fun PengelolaHalaman(
                     navController.navigate(DestinasiHome.route)
                 }
             )
+        }
+        composable(
+            DestinasiDetail.routesWithArg,
+            arguments = listOf(
+                navArgument(DestinasiDetail.NIM){
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val nim = it.arguments?.getString(DestinasiDetail.NIM)
+
+            nim?.let { nim ->
+                DetailMhsView(
+                    onBack = {
+                        navController.popBackStack()
+                    }
+                )
+            }
         }
     }
 }
